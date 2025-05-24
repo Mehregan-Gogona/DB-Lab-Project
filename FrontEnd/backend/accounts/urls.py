f646b92a1796220314f846923d03b06738aa2722
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-05-24 17:46:35 +0330

    profile change fixe

--- FrontEnd/backend/accounts/urls.py
+++ FrontEnd/backend/accounts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
-import os
-from django.conf import settings
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
@@ -10,4 +8,4 @@ urlpatterns = [
     path('dashboard/tasks/', views.get_tasks, name='get_tasks'),   
     path('update_profile/', views.update_profile, name='update_profile'),
     path('get_profile/', views.get_profile, name='get_profile'),
-] + static('/media/', document_root=os.path.join(settings.BASE_DIR, 'media'))
+]
